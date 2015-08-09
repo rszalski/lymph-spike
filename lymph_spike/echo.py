c35ab4fddad0a7ffe145f2bd9505db13cf5c2d3c
Radosław Szalski <radoslaw.szalski@gmail.com>
2015-08-09 20:26:13 +0200

    Adds simple echo and greetings services

---
+++ lymph_spike/echo.py
@@ -0,0 +1,13 @@
+import lymph
+
+
+class Echo(lymph.Interface):
+    @lymph.rpc
+    def echo(self, text=None):
+        return text
+
+    @lymph.rpc
+    def greet(self, name):
+        print('Saying Hi!')
+        self.emit('greeted', {'result': 'Said hi!'})
+        return 'Hi, {}!'.format(name)
